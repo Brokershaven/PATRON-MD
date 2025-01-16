d7b7ead5ce931e67db34e5aaf757b94c79389bc3
Brokershaven <141914931+Brokershaven@users.noreply.github.com>
2025-01-16 02:27:05 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "GTB"
 
-global.banknumber = "8133729715"
+global.banknumber = "346267478"
 
-global.bankowner = "FADARE"
+global.bankowner = "A-TOBTIM"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
